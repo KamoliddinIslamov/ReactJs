3022307e4b2459ff9a5977fd2970cce2bacc0a93
KamoliddinIslamov <kamoliddin07@outlook.com>
2022-11-04 14:48:31 +0500

    React js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Group from "./imgs/Group.png"
+import Group1 from "./imgs/Group1.png"
+import Group2 from "./imgs/Group2.png"
+import img from "./imgs/img.png"
+import img1 from "./imgs/img1.png"
+import img2 from "./imgs/img2.png"
+import Vector from "./imgs/Vector.png"
+import Vector1 from "./imgs/Vector1.png"
+import zoom from "./imgs/zoom.png"
+import MapProyect from './MapProyect';
+import MapProyect1 from './MapProyect1';
+import MapProyect2 from './MapProyect2';
 function App() {
+  let data = [{ titleLeft: "FEATURED", titleRight: "FOR SALE", src: img, title: "New Apartment Nice Wiew", text: "Quincy St, Brooklyn, NY, USA", src1: Group, src2: Group1, src3: Vector, src4: Group2, commit: "4 Beds", commit2: "5 Baths", commit3: "1 Garage", commit4: "1200 Sq Ft", price: "$2,800/mo", newPrice: "$7,500/mo", zoomImg: zoom, love: Vector1 }]
+
+  let data1 = [{ titleLeft: "FEATURED", titleRight: "FOR SALE", src: img1, title: "New Apartment Nice Wiew", text: "Quincy St, Brooklyn, NY, USA", src1: Group, src2: Group1, src3: Vector, src4: Group2, commit: "4 Beds", commit2: "5 Baths", commit3: "1 Garage", commit4: "1200 Sq Ft", price: "$2,800/mo", newPrice: "$7,500/mo", zoomImg: zoom, love: Vector1 }]
+
+  let data2 = [{ titleLeft: "FEATURED", titleRight: "FOR SALE", src: img2, title: "New Apartment Nice Wiew", text: "Quincy St, Brooklyn, NY, USA", src1: Group, src2: Group1, src3: Vector, src4: Group2, commit: "4 Beds", commit2: "5 Baths", commit3: "1 Garage", commit4: "1200 Sq Ft", price: "$2,800/mo", newPrice: "$7,500/mo", zoomImg: zoom, love: Vector1 }]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapProyect xabar={data} />
+      <MapProyect1 xabar={data1} />
+      <MapProyect2 xabar={data2} />
     </div>
   );
 }
---
+++ src/MapProyect2.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./MapProyect.css";
+class MapProyect2 extends React.Component {
+    render() {
+        return (
+            <div className="wrapper">
+                {this.props.xabar.map(value => (
+                    <div className="container">
+                        <div className="bg_img">
+                            <img src={value.src} />
+                            <div className="btns">
+                                <button>{value.titleLeft}</button>
+                                <button>{value.titleRight}</button>
+                            </div>
+                        </div>
+                        <div className="main">
+                            <h2>{value.title}</h2>
+                            <h3>{value.text}</h3>
+                            <div className="iconks">
+                                <div className="box">
+                                    <div className="iconc"><img src={value.src1} /></div>
+                                    <h3>{value.commit}</h3>
+                                </div>
+                                <div className="box">
+                                    <div className="iconc"><img src={value.src2} /></div>
+                                    <h3>{value.commit2}</h3>
+                                </div>
+                                <div className="box">
+                                    <div className="iconc"><img src={value.src3} /></div>
+                                    <h3>{value.commit3}</h3>
+                                </div>
+                                <div className="box">
+                                    <div className="iconc"><img src={value.src4} /></div>
+                                    <h3>{value.commit4}</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="main_footer">
+                            <div className="footer">
+                                <h3><del>{value.price}</del></h3>
+                                <h2>{value.newPrice}</h2>
+                            </div>
+                            <div className="iconk_img">
+                                <img src={value.zoomImg} /><img src={value.love} />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
+export default MapProyect2
